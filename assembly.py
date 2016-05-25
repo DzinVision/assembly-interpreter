1b06e22bb18e261d6ee5c63058c97181727f6fb8
Vid Drobnič <vid.drobnic@gmail.com>
2016-05-25 07:42:03 -0400

    Working commit

--- assembly.py
+++ assembly.py
@@ -10,102 +10,100 @@ labels = {}
 current_line = 0
 
 def load(a):
-    global acc, labels
+    global acc, labels, current_line, variables
     acc = variables[a]
 
 def store(variable):
-    global acc, labels
+    global acc, labels, current_line, variables
     variables[variable] = acc
 
 def add(variable):
-    global acc, labels
+    global acc, labels, current_line, variables
     if variable[0] == '=':
         acc += int(variable[1:])
     else:
         acc += variables[variable]
 
 def sub(variable):
-    global acc, labels
+    global acc, labels, current_line, variables
     if variable[0] == '=':
         acc -= int(variable[1:])
     else:
         acc -= variables[variable]
 
 def mult(variable):
-    global acc, labels
+    global acc, labels, current_line, variables
     if variable[0] == '=':
         acc *= int(variable[1:])
     else:
         acc *= variables[variable]
 
 def div(variable):
-    global acc, labels
+    global acc, labels, current_line, variables
     if variable[0] == '=':
         acc //= int(variable[1:])
     else:
         acc //= variables[variable]
 
 def be(label):
-    global acc, labels
+    global acc, labels, current_line, variables
     if acc == 0:
         current_line = labels[label] - 1
 
 def bg(label):
-    global acc, labels
+    global acc, labels, current_line, variables
     if acc > 0:
         current_line = labels[label] - 1
 
 def bl(label):
-    global acc, labels
+    global acc, labels, current_line, variables
     if acc < 0:
         current_line = labels[label] - 1
 
 def bu(label):
-    global acc, labels
+    global acc, labels, current_line, variables
     current_line = labels[label] - 1
 
-def end():
-    global acc, labels
+def end(a):
+    global acc, labels, current_line, variables
     current_line = -2
 
 def read(variable):
-    global acc, labels
-    variables[variable] = int(input())
+    global acc, labels, current_line, variables
+    value = int(input())
+    variables[variable] = value
+    acc = value
 
 def assembly_print(variable):
-    global acc, labels
+    global acc, labels, current_line, variables
     print(variables[variable])
 
 def dc(variable, value):
-    global acc, labels
+    global acc, labels, current_line, variables
     variables[variable] = int(value)
 
 
-code = [i.lower() for i in code]
+code = [i.lower().replace('\n', '') for i in code]
 
 for i, line in enumerate(code):
-    parameters = line.split()
-    if len(parameters) == 3 and parameters[1] != 'dc':
+    parameters = line.split('\t')
+    if parameters[0] != '':
         labels[parameters[0]] = i
 
-print(labels)
-
 while current_line > -1 and current_line < len(code):
-    parameters = code[current_line].split()
+    parameters = code[current_line].split('\t')
 
-    index_add = 0
-
-    if len(parameters) == 3 and parameters[1] == 'dc':
+    if parameters[1] == 'dc':
         dc(parameters[0], parameters[2])
         current_line += 1
         continue
 
-    if len(parameters) == 3:
-        index_add = 1
+    if parameters[1] == 'print':
+        assembly_print(parameters[2])
+        current_line += 1
+        continue
 
-    parameters[index_add] = parameters[index_add].replace('print', 'assembly_print')
-
-    eval_string = parameters[index_add] + '("' + parameters[1+index_add] + '")'
+    eval_string = parameters[1] + '("' + parameters[2] + '")'
     eval(eval_string)
 
     current_line += 1
